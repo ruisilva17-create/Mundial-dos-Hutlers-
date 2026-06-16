b4cdc2dc73162aaed1c1e3be6696dccd7e7b2379
ruisilva17-create <ruisilva17@icloud.com>
2026-06-16 11:11:28 +0100

    Create test-fixtures.js

---
+++ api/test-fixtures.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  const key = process.env.API_FOOTBALL_KEY;
+
+  const response = await fetch(
+    'https://v3.football.api-sports.io/fixtures?date=2026-06-11',
+    {
+      headers: {
+        'x-apisports-key': key
+      }
+    }
+  );
+
+  const data = await response.json();
+  return res.status(200).json(data);
+}
